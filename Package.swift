38c3195eddaef58af8def1f3878ff7ce51b8b986
manuel <manuelcarloslopes@icloud.com>
2022-06-19 22:56:49 +0200

    Remove comments from package manifest

--- Package.swift
+++ Package.swift
@@ -7,15 +7,12 @@ let package = Package(name: "RateLimiters",
                       platforms: [
                         .iOS("16.0")
                       ],
-                      // Products define the executables and libraries produced by a package, and make them visible to other packages.
+
                       products: [
                         .library(name: "Throttler", targets: ["Throttler"]),
                         .library(name: "Debouncer", targets: ["Debouncer"])
                       ],
 
-                      // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-                      // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-                      // MARK: - Public API
                       targets: [
                         .target(name: "Throttler"),
                         .target(name: "Debouncer"),
@@ -23,6 +20,7 @@ let package = Package(name: "RateLimiters",
                         .testTarget(name: "ThrottlerTests", dependencies: ["Throttler"]),
                         .testTarget(name: "DebouncerTests", dependencies: ["Debouncer"])
                       ],
+
                       swiftLanguageVersions: [
                         .v5
                       ])
