59a745864cb0c8f0291c54e70cf56519518d6958
Manuel Lopes <manuelcarloslopes@icloud.com>
2024-10-31 19:24:31 +0100

    wip

--- Package.swift
+++ Package.swift
@@ -2,29 +2,6 @@
 
 import PackageDescription
 
-let swiftSettings: [SwiftSetting] = [
-   .enableUpcomingFeature("BareSlashRegexLiterals"),
-   .enableUpcomingFeature("ConciseMagicFile"),
-   .enableUpcomingFeature("ExistentialAny"),
-   .enableUpcomingFeature("ForwardTrailingClosures"),
-   .enableUpcomingFeature("ImplicitOpenExistentials"),
-   .enableUpcomingFeature("StrictConcurrency"),
-   .enableUpcomingFeature("ImportObjcForwardDeclarations"),
-   .enableUpcomingFeature("DisableOutwardActorInference"),
-   .enableUpcomingFeature("InternalImportsByDefault"),
-   .enableUpcomingFeature("IsolatedDefaultValues"),
-   .enableUpcomingFeature("GlobalConcurrency"),
-   .enableExperimentalFeature("StrictConcurrency"),
-   .unsafeFlags([
-    "-warn-concurrency",
-    "-enable-actor-data-race-checks",
-    "-Xfrontend",
-    "-warn-long-function-bodies=50",
-    "-Xfrontend",
-    "-warn-long-expression-type-checking=100"
-   ])
-]
-
 let package = Package(name: "RateLimiters",
 
                       products: [
@@ -34,12 +11,10 @@ let package = Package(name: "RateLimiters",
 
                       targets: [
                         .target(name: "Throttler",
-                                resources: [.process("PrivacyInfo.xcprivacy")],
-                                swiftSettings: swiftSettings
+                                resources: [.process("PrivacyInfo.xcprivacy")]
                                ),
                         .target(name: "Debouncer",
-                                resources: [.process("PrivacyInfo.xcprivacy")],
-                                swiftSettings: swiftSettings
+                                resources: [.process("PrivacyInfo.xcprivacy")]
                                ),
 
                         .testTarget(name: "ThrottlerTests", dependencies: ["Throttler"]),
